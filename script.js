5d42df6bb58d1c0085a346b4e6d3f1fba5db38ef
Aashna-Verma <aashna.verma@outlook.com>
2022-06-19 18:49:11 -0400

    Final touches complete

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//nothing here yet!
+function copyEmail(id) {
+    let cText  = document.getElementById(id);
+
+    navigator.clipboard.writeText(cText.innerHTML);
+
+    alert("Copied the text: " + cText.innerHTML);
+}
