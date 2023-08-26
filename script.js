8937c88eeb79abe0775b2f6f0d6a46c0d69dce7c
Aashna-Verma <aashna.verma@outlook.com>
2023-08-26 00:51:12 -0400

    fancy tooltip

--- script.js
+++ script.js
@@ -5,3 +5,52 @@ function copyEmail(id) {
 
     alert("Copied the text: " + cText.innerHTML);
 }
+
+//-----------------------ROTATING CURSOR---------------------------//
+const follower = document.querySelector(".svg-prac");
+
+//-------------------CURSOR-----------------//
+
+const cursor = document.querySelector(".cursor");
+let selected = document.querySelectorAll(".fancy-selector");
+
+const coords = { x: 0, y: 0};
+
+document.addEventListener("mousemove", (e) => {
+    let prevX = coords.x;
+    let prevY = coords.y;
+
+    coords.x = e.pageX - 10;
+    coords.y = e.pageY - 10;
+    
+    let x = coords.x;
+    let y = coords.y;
+
+    x += (prevX - x);
+    y += (prevY - y);
+
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+    follower.style.left = (x - 15);
+    follower.style.top = (y - 15);
+    
+});
+
+selected.forEach(item => {
+    item.addEventListener("mouseenter", (e) => {
+        cursor.classList.add("active");
+        document.querySelector("textPath").innerHTML = item.getAttribute("name");
+        follower.classList.add("active");
+    });
+});
+
+selected.forEach(item => {
+    item.addEventListener("mouseleave", (e) => {
+        cursor.classList.remove("active");
+        follower.classList.remove("active");
+    });
+});
+
+
+
+    
