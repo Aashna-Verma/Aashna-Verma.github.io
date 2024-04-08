f6ff5b7eab9db9f68bc13b65018beb34a5bd5133
Aashna-Verma <aashna.verma@outlook.com>
2024-04-07 23:13:03 -0400

    %1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,14 +29,13 @@ function Home() {
 				gsap.timeline({
 					scrollTrigger: {
 						trigger: upsie,
-						start: 'bottom bottom',
-						end: 'top bottom',
+						start: 'top bottom',
+						end: '+=100 bottom',
 						scrub: true,
-						markers: false
+						markers: true
 					}
 				}).to(upsie, {
-					y: 0,
-					duration: .1
+					y: 0
 				});
 			});
 
@@ -50,10 +49,7 @@ function Home() {
 						markers: false
 					}
 				}).to(upsie, {
-					y: 0,
-					stagger: 0.05,
-					delay: 1,
-					duration: 1
+					y: 0
 				});
 			});
 		}, 50);
@@ -61,12 +57,15 @@ function Home() {
 	});
 
 	// headers for each section
-	const header = (a, b) => {
+	const header = (a, b, c) => {
 		return (
 			<div className='clipy'>
-				<div className='upsie home-header'>
+				<div className='upsie header-paragraph'>
+					<div className='home-header'>
 					<h2 className='header-number'>{a}</h2>
 					<h2 className='header-title'>{b}</h2>
+					</div>
+					<p className='header-p'>{c}</p>
 				</div>
 			</div>
 		);
@@ -92,27 +91,23 @@ function Home() {
 
 			<Hero className='home-section' />
 
-			<div className='home-section'>
-				{header('./1', 'about me')}
-				<div id='home-about'>
-					<p className='line-popup about-p'>I'm a Software Engineering student @ CARLETON UNIVERSITY. Merging creativity with technology to
-						create functional and aesthetic systems.</p>
+			<div className='home-section' id='home-about'>
+				{header('./01', 'About', 'I\'m a Software Engineering student @ CARLETON UNIVERSITY. Merging creativity with technology to create functional and aesthetic systems.')}
+				{column2('Skills', 'Shaped by my academic personal passions. This is a small peek into my continuous learning journey.', ['FullStack', 'UI/UX Design', 'Web Dev', 'Graphic Design'])}
+				{column2('Technologies', 'Over time, I\'ve cultivated this skill set, shaped by my academic pursuits at university and my personal passions. This is a small peek into my continuous learning journey.', ['JS/HTML/CSS', 'ReactJS', 'Blazor', 'Python', 'Java', 'Figma'])}
 
-					{column2('Skills', 'Shaped by my academic personal passions. This is a small peek into my continuous learning journey.', ['FullStack', 'UI/UX Design', 'Web Dev', 'Graphic Design'])}
-					{column2('Technologies', 'Over time, I\'ve cultivated this skill set, shaped by my academic pursuits at university and my personal passions. This is a small peek into my continuous learning journey.', ['JS/HTML/CSS', 'ReactJS', 'Blazor', 'Python', 'Java', 'Figma'])}
 
-				</div>
 			</div>
 
 			<div className='home-section' id='home-projects'>
-				{header('./2', 'recent projects')}
-				<Project img={img} title='ColorMe' tags={['FrontEnd', 'BackEnd', 'turkey', 'chciken', 'sdjfhskjdhfskd']} links={[['Github', 'https://github.com/Isabella-Nguyen/ColourMe']]}/>
-				<Project img={img} title='cuHacking' tags={['FrontEnd', 'Back']}  />
-				<Project img={img} title='ColorMe' tags={['FrontEnd', 'Back']} links={[['Github', 'https://github.com/Isabella-Nguyen/ColourMe']]}/>
+				{header('./02', 'Recent projects')}
+				<Project img={img} title='ColorMe' tags={['FrontEnd', 'BackEnd', 'turkey', 'chciken', 'sdjfhskjdhfskd']} links={[['Github', 'https://github.com/Isabella-Nguyen/ColourMe']]} />
+				<Project img={img} title='cuHacking' tags={['FrontEnd', 'Back']} />
+				<Project img={img} title='ColorMe' tags={['FrontEnd', 'Back']} links={[['Github', 'https://github.com/Isabella-Nguyen/ColourMe']]} />
 			</div>
 
 			<div id='home-projects'>
-				{header('./3', 'contact me')}
+				{header('./03', 'contact me')}
 			</div>
 
 			<div className='space'></div>
